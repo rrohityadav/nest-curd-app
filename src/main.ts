25feb61ab167d0b882ed44efd8361777cfd40104
Rohit <rohityadav20101998@gmail.com>
2024-11-04 15:22:59 +0530

    add sample env file

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(2000, () => {
-    console.log('App listening on port 2000');
+  const port = process.env.PORT || 3000; // Default to 3000 if PORT is not defined
+  await app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
   });
 }
 bootstrap();
